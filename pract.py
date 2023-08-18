5a15d97984a02e8c108c160ea7bc3e06b938434c
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-18 11:08:52 +0530

    Update pract.py

--- pract.py
+++ pract.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
-st.header('This is a header')
-st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+def main():
+    st.title("Streamlit Navbar with Dropdown")
+    
+    # Create a navigation menu with options
+    menu_options = ["Home", "About", "Services", "Contact"]
+    selected_option = st.sidebar.selectbox("Select an option", menu_options)
+    
+    # Display content based on the selected option
+    if selected_option == "Home":
+        st.write("Welcome to the Home page!")
+    elif selected_option == "About":
+        st.write("This is the About page. Learn more about us here.")
+    elif selected_option == "Services":
+        st.write("Explore our services and what we offer.")
+    elif selected_option == "Contact":
+        st.write("Contact us if you have any questions or inquiries.")
+
+if __name__ == "__main__":
+    main()
