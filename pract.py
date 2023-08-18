73976d75b66850cc260815f43c15cbc7ebb365e8
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-18 11:17:04 +0530

    Update pract.py

--- pract.py
+++ pract.py
@@ -1,1 +1,20 @@
+import streamlit as st
+
+def main():
+    st.title("Simple Streamlit Navbar")
+
+    menu = ["Home", "About", "Services", "Contact"]
+    selected_page = st.radio("Navigate", menu)
+
+    if selected_page == "Home":
+        st.write("Welcome to the Home page!")
+    elif selected_page == "About":
+        st.write("This is the About page. Learn more about us here.")
+    elif selected_page == "Services":
+        st.write("Explore our services and what we offer.")
+    elif selected_page == "Contact":
+        st.write("Contact us if you have any questions or inquiries.")
+
+if __name__ == "__main__":
+    main()
 
