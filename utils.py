a8649964b85cfe598aea093d0bfc689166575f2f
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-18 13:04:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,9 +4,6 @@ from streamlit.components.v1 import html
 
 from PATHS import NAVBAR_PATHS, SETTINGS
 
-# Your model's title
-model_title = "Your Model Title"
-
 def inject_custom_css():
     with open('assets/styles.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -17,69 +14,54 @@ def get_current_route():
     except:
         return None
 
-def navbar_component():
-    with open("assets/images/settings.png", "rb") as image_file:
-        image_as_base64 = base64.b64encode(image_file.read())
+# Your model's title
+model_title = "Your Model Title"
 
-    navbar_items = ''
-    for key, value in NAVBAR_PATHS.items():
-        navbar_items += (f'<a class="navitem" href="/?nav={value}">{key}</a>')
+# Display the title for the dropdown
+st.title(model_title)
 
-    settings_items = ''
-    for key, value in SETTINGS.items():
-        settings_items += (
-            f'<a href="/?nav={value}" class="settingsNav">{key}</a>')
+# Display the custom navigation bar
+with open("assets/images/settings.png", "rb") as image_file:
+    image_as_base64 = base64.b64encode(image_file.read())
 
-    component = rf'''
-            <nav class="container navbar" id="navbar">
-                <ul class="navlist">
-                {navbar_items}
-                </ul>
-                <div class="dropdown" id="settingsDropDown">
-                    <img class="dropbtn" src="data:image/png;base64, {image_as_base64.decode("utf-8")}"/>
-                    <div id="myDropdown" class="dropdown-content">
-                        {settings_items}
-                    </div>
-                </div>
-            </nav>
-            '''
-    st.markdown(component, unsafe_allow_html=True)
-    js = '''
-    <script>
-        // navbar elements
-        var navigationTabs = window.parent.document.getElementsByClassName("navitem");
-        var cleanNavbar = function(navigation_element) {
-            navigation_element.removeAttribute('target')
-        }
-        
-        for (var i = 0; i < navigationTabs.length; i++) {
-            cleanNavbar(navigationTabs[i]);
-        }
-        
-        // Dropdown hide / show
-        var dropdown = window.parent.document.getElementById("settingsDropDown");
-        dropdown.onclick = function() {
-            var dropWindow = window.parent.document.getElementById("myDropdown");
-            if (dropWindow.style.visibility == "hidden"){
-                dropWindow.style.visibility = "visible";
-            }else{
-                dropWindow.style.visibility = "hidden";
-            }
-        };
-        
-        var settingsNavs = window.parent.document.getElementsByClassName("settingsNav");
-        var cleanSettings = function(navigation_element) {
-            navigation_element.removeAttribute('target')
-        }
-        
-        for (var i = 0; i < settingsNavs.length; i++) {
-            cleanSettings(settingsNavs[i]);
-        }
-    </script>
-    '''
-    html(js)
+settings_items = ''
+for key, value in SETTINGS.items():
+    settings_items += (
+        f'<a href="/?nav={value}" class="settingsNav">{key}</a>')
 
-st.title(model_title)  # Display the title of the model
-navbar_component()  # Display the custom navigation bar
+dropdown_component = rf'''
+        <div class="dropdown" id="settingsDropDown">
+            <img class="dropbtn" src="data:image/png;base64, {image_as_base64.decode("utf-8")}"/>
+            <div id="myDropdown" class="dropdown-content">
+                {settings_items}
+            </div>
+        </div>
+        '''
+st.markdown(dropdown_component, unsafe_allow_html=True)
+
+js = '''
+<script>
+    // Dropdown hide / show
+    var dropdown = window.parent.document.getElementById("settingsDropDown");
+    dropdown.onclick = function() {
+        var dropWindow = window.parent.document.getElementById("myDropdown");
+        if (dropWindow.style.visibility == "hidden"){
+            dropWindow.style.visibility = "visible";
+        }else{
+            dropWindow.style.visibility = "hidden";
+        }
+    };
+
+    var settingsNavs = window.parent.document.getElementsByClassName("settingsNav");
+    var cleanSettings = function(navigation_element) {
+        navigation_element.removeAttribute('target')
+    }
+
+    for (var i = 0; i < settingsNavs.length; i++) {
+        cleanSettings(settingsNavs[i]);
+    }
+</script>
+'''
+html(js)
 
 # Your other Streamlit code here
