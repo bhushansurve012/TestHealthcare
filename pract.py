f567ea6028916e3e2ae840933e99ba15d7d55272
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-18 11:09:57 +0530

    Update pract.py

--- pract.py
+++ pract.py
@@ -1,19 +1,25 @@
 import streamlit as st
 
 def main():
-    st.title("Streamlit Navbar with Dropdown")
-    
-    # Create a navigation menu with options
-    menu_options = ["Home", "About", "Services", "Contact"]
-    selected_option = st.sidebar.selectbox("Select an option", menu_options)
-    
-    # Display content based on the selected option
+    st.title("Streamlit Horizontal Navbar with Dropdown")
+
+    menu_options = {
+        "Home": [],
+        "About": [],
+        "Services": ["Service 1", "Service 2", "Service 3"],
+        "Contact": []
+    }
+
+    selected_option = st.sidebar.radio("Select an option", list(menu_options.keys()))
+
     if selected_option == "Home":
         st.write("Welcome to the Home page!")
     elif selected_option == "About":
         st.write("This is the About page. Learn more about us here.")
     elif selected_option == "Services":
         st.write("Explore our services and what we offer.")
+        selected_service = st.selectbox("Select a service", menu_options["Services"])
+        st.write(f"You selected: {selected_service}")
     elif selected_option == "Contact":
         st.write("Contact us if you have any questions or inquiries.")
 
