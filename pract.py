4d976f24a159d2ce1ea0d94595973581a5f85ed7
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-18 11:18:03 +0530

    Update pract.py

--- pract.py
+++ pract.py
@@ -1,20 +1,35 @@
 import streamlit as st
 
 def main():
-    st.title("Simple Streamlit Navbar")
+    st.title("Custom Horizontal Navbar")
 
     menu = ["Home", "About", "Services", "Contact"]
-    selected_page = st.radio("Navigate", menu)
+    selected_page = st.radio("", menu)
 
-    if selected_page == "Home":
-        st.write("Welcome to the Home page!")
-    elif selected_page == "About":
-        st.write("This is the About page. Learn more about us here.")
-    elif selected_page == "Services":
-        st.write("Explore our services and what we offer.")
-    elif selected_page == "Contact":
-        st.write("Contact us if you have any questions or inquiries.")
-
-if __name__ == "__main__":
-    main()
-
+    # Create a custom horizontal navbar
+    st.markdown(
+        """
+        <style>
+            .navbar {
+                display: flex;
+                justify-content: center;
+                background-color: #f1f1f1;
+                padding: 1rem;
+                margin-bottom: 1rem;
+            }
+            .navbar a {
+                padding: 0.5rem 1rem;
+                text-decoration: none;
+                color: #333;
+                font-weight: bold;
+                border: 1px solid transparent;
+            }
+            .navbar a:hover {
+                background-color: #ddd;
+                border: 1px solid #333;
+            }
+        </style>
+        <div class="navbar">
+            <a href="#" class="active">Home</a>
+            <a href="#">About</a>
+            <a href
